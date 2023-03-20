c3d50b1b56540ee6157983046351bbc25efbfdea
Abaljerind <abaljerinda@gmail.com>
2023-03-20 18:16:33 +0700

    add: contoh if statement & if dgn math random

--- javascript/latihan1/app.js
+++ javascript/latihan1/app.js
@@ -1,1 +1,19 @@
 console.log("ini contoh penggunaan javascript di dalam file");
+
+// contoh penggunaan if pada javascript
+if (1 + 1 === 3) {
+  console.log("nilai 1 + 1 = 2 sudah betul!");
+}
+
+// contoh if dengan method math random
+let angka = Math.random();
+
+console.log(angka);
+
+if (angka >= 0.05) {
+  console.log("angka lebih besar dari pada 0.05");
+}
+
+if (angka <= 0.5) {
+  console.log("angka lebih kecil dari pada 0.05");
+}
