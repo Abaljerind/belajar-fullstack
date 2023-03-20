7f03e7d3a694b67a55381029217df8984f145bcf
Abaljerind <abaljerinda@gmail.com>
2023-03-20 21:16:53 +0700

    add: nested if u/ pengecekan password

--- javascript/latihan1/app.js
+++ javascript/latihan1/app.js
@@ -1,6 +1,7 @@
 console.log("ini contoh penggunaan javascript di dalam file");
 
 // contoh penggunaan if pada javascript
+console.log("contoh penggunaan if pada js");
 if (1 + 1 === 3) {
   console.log("nilai 1 + 1 = 2 sudah betul!");
 }
@@ -11,12 +12,14 @@ let angka = Math.random();
 console.log(angka);
 
 if (angka >= 0.05) {
-  console.log("angka lebih besar dari pada 0.05");
+  console.log("Contoh if dgn Math, angka lebih besar dari pada 0.05");
 }
 
 if (angka <= 0.5) {
-  console.log("angka lebih kecil dari pada 0.05");
+  console.log("Contoh if dgn Math, angka lebih kecil dari pada 0.05");
 }
+
+console.log();
 
 // contoh else if statement
 console.log("contoh contoh penggunaan else if");
@@ -28,6 +31,8 @@ if (iniHari === "senin") {
 } else if (iniHari === "sabtu") {
   console.log("semoga tidak cepat berlalu!");
 }
+
+console.log();
 
 const nilai = "40";
 
@@ -43,6 +48,8 @@ if (nilai < 50) {
   console.log("A");
 }
 
+console.log();
+
 // contoh penggunaan else
 console.log("contoh penggunaan else untuk opsi terakhir");
 
@@ -54,4 +61,23 @@ if (hariIni === "senin") {
   console.log("semoga tidak cepat berlalu!");
 } else {
   console.log("yah biasa ajalah!");
+}
+
+console.log();
+
+// contoh nested if / if berlapis untuk pengecekan password
+console.log("contoh penggunaan nested if");
+
+const password = prompt("silahkan masukkan password!");
+
+// password harus memilik panjang 6 karakter
+if (password.length >= 6) {
+  // mengecek apakah password memiliki spasi di dalamnya
+  if (password.indexOf(" ") === -1) {
+    console.log("password valid");
+  } else {
+    console.log("Tidak boleh ada spasi!");
+  }
+} else {
+  console.log("password invalid! Minimal harus 6 karakter");
 }
