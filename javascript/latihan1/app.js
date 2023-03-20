552bee4810ee5f726166b3740546a9140bc3c640
Abaljerind <abaljerinda@gmail.com>
2023-03-20 20:45:16 +0700

    add: contoh penggunaan else if statement

--- javascript/latihan1/app.js
+++ javascript/latihan1/app.js
@@ -17,3 +17,28 @@ if (angka >= 0.05) {
 if (angka <= 0.5) {
   console.log("angka lebih kecil dari pada 0.05");
 }
+
+// contoh else if statement
+console.log("contoh contoh penggunaan else if");
+
+const iniHari = "sabtu";
+
+if (iniHari === "senin") {
+  console.log("selamat bekerja lagi!");
+} else if (iniHari === "sabtu") {
+  console.log("semoga tidak cepat berlalu!");
+}
+
+const nilai = "40";
+
+if (nilai < 50) {
+  console.log("E");
+} else if (nilai < 60) {
+  console.log("D");
+} else if (nilai < 70) {
+  console.log("C");
+} else if (nilai < 80) {
+  console.log("B");
+} else if (nilai < 100) {
+  console.log("A");
+}
