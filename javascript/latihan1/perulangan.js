9dca4c5bf7b1dee80e8afdb43555ff1ef62cf94d
Abaljerind <abaljerinda@gmail.com>
2023-03-21 21:19:53 +0700

    add: contoh for loop dgn increment - dan *

--- javascript/latihan1/perulangan.js
+++ javascript/latihan1/perulangan.js
@@ -1,5 +1,31 @@
 console.log("contoh perulangan");
 
-for (let x = 1; x <= 10; x++) {
-  console.log("perulangan ke - " + x);
+// for (let x = 1; x <= 10; x++) {
+//   console.log("perulangan ke - " + x);
+// }
+
+// perulangan dengan ganjil dan genap
+// for (let y = 0; y <= 20; y += 2) {
+//   console.log("perulangan genap ke - " + y);
+// }
+
+// for (let z = 1; z <= 20; z += 2) {
+//   console.log("perulangan ganjil ke - " + z);
+// }
+
+// for (let x = 20; x >= 0; x--) {
+//   console.log("perulangan ke - " + x);
+// }
+
+// for (let y = 20; y >= 0; y -= 2) {
+//   console.log("perulangan genap ke - " + y);
+// }
+
+// for (let z = 19; z >= 0; z -= 2) {
+//   console.log("perulangan ganjil ke - " + z);
+// }
+
+// perulangan dengan increment perkalian
+for (let i = 10; i <= 100000; i *= 10) {
+  console.log("perulangan perkalian ke - " + i);
 }
