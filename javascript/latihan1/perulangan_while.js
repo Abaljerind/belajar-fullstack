edae9360c01ecf57b0784a628dcc6af463702b65
Abaljerind <abaljerinda@gmail.com>
2023-03-22 06:43:43 +0700

    add: contoh while loop sederhana

--- javascript/latihan1/perulangan_while.js
+++ javascript/latihan1/perulangan_while.js
@@ -1,1 +1,18 @@
 console.log("perulangan dengan while");
+
+// contoh while loop sederhana
+let num = 0;
+while (num < 10) {
+  console.log(num);
+  num++;
+}
+
+let password = "password";
+
+let user = prompt("masukkan password");
+
+while (user !== password) {
+  user = prompt("masukkan password");
+}
+
+alert("selamat datang");
