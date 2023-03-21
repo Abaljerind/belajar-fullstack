fb345620980e7c36df2cc1a3cf3434b5727fa54b
Abaljerind <abaljerinda@gmail.com>
2023-03-21 08:34:50 +0700

    add: contoh penggunaan AND, OR, NOT, & SWITCH

--- javascript/latihan1/app.js
+++ javascript/latihan1/app.js
@@ -22,62 +22,117 @@ if (angka <= 0.5) {
 console.log();
 
 // contoh else if statement
-console.log("contoh contoh penggunaan else if");
+// console.log("contoh contoh penggunaan else if");
 
-const iniHari = "rabu";
+// const iniHari = "rabu";
 
-if (iniHari === "senin") {
-  console.log("selamat bekerja lagi!");
-} else if (iniHari === "sabtu") {
-  console.log("semoga tidak cepat berlalu!");
-}
+// if (iniHari === "senin") {
+//   console.log("selamat bekerja lagi!");
+// } else if (iniHari === "sabtu") {
+//   console.log("semoga tidak cepat berlalu!");
+// }
 
-console.log();
+// console.log();
 
-const nilai = "40";
+// const nilai = "40";
 
-if (nilai < 50) {
-  console.log("E");
-} else if (nilai < 60) {
-  console.log("D");
-} else if (nilai < 70) {
-  console.log("C");
-} else if (nilai < 80) {
-  console.log("B");
-} else if (nilai < 100) {
-  console.log("A");
-}
+// if (nilai < 50) {
+//   console.log("E");
+// } else if (nilai < 60) {
+//   console.log("D");
+// } else if (nilai < 70) {
+//   console.log("C");
+// } else if (nilai < 80) {
+//   console.log("B");
+// } else if (nilai < 100) {
+//   console.log("A");
+// }
 
-console.log();
+// console.log();
 
 // contoh penggunaan else
-console.log("contoh penggunaan else untuk opsi terakhir");
+// console.log("contoh penggunaan else untuk opsi terakhir");
 
-const hariIni = prompt("isikan hari ini!").toLowerCase();
+// const hariIni = prompt("isikan hari ini!").toLowerCase();
 
-if (hariIni === "senin") {
-  console.log("selamat bekerja lagi!");
-} else if (hariIni === "sabtu") {
-  console.log("semoga tidak cepat berlalu!");
-} else {
-  console.log("yah biasa ajalah!");
-}
+// if (hariIni === "senin") {
+//   console.log("selamat bekerja lagi!");
+// } else if (hariIni === "sabtu") {
+//   console.log("semoga tidak cepat berlalu!");
+// } else {
+//   console.log("yah biasa ajalah!");
+// }
+
+// console.log();
+
+// contoh nested if / if berlapis untuk pengecekan password
+// console.log("contoh penggunaan nested if");
+
+// const password = prompt("silahkan masukkan password!");
+
+// // password harus memilik panjang 6 karakter
+// if (password.length >= 6) {
+//   // mengecek apakah password memiliki spasi di dalamnya
+//   if (password.indexOf(" ") === -1) {
+//     console.log("password valid");
+//   } else {
+//     console.log("Tidak boleh ada spasi!");
+//   }
+// } else {
+//   console.log("password invalid! Minimal harus 6 karakter");
+// }
+
+// console.log();
+
+// contoh penggunaan operator logic AND
+// console.log("contoh penggunaan operator AND");
+
+// if (password.length >= 6 && password.indexOf(" ") === -1) {
+//   console.log("password valid!");
+// } else {
+//   console.log("password tidak memenuhi syarat");
+// }
+
+// console.log();
+
+// contoh penggunaan operator logic OR
+// console.log("contoh penggunaan operator OR");
+
+// const role = prompt("masukkan data akses anda!");
+
+// if (role === "admin" || role === "spv") {
+//   console.log("selamat datang.");
+// } else {
+//   console.log("akses ditolak!");
+// }
 
 console.log();
 
-// contoh nested if / if berlapis untuk pengecekan password
-console.log("contoh penggunaan nested if");
+// contoh penggunaan operator logic NOT
+// console.log("contoh penggunaan operator NOT");
 
-const password = prompt("silahkan masukkan password!");
+// const role = prompt("masukkan data akses anda");
 
-// password harus memilik panjang 6 karakter
-if (password.length >= 6) {
-  // mengecek apakah password memiliki spasi di dalamnya
-  if (password.indexOf(" ") === -1) {
-    console.log("password valid");
-  } else {
-    console.log("Tidak boleh ada spasi!");
-  }
-} else {
-  console.log("password invalid! Minimal harus 6 karakter");
+// if (role != "admin") {
+//   console.log("akses ditolak!");
+// } else {
+//   console.log("selamat datang admin");
+// }
+
+// contoh penggunaan switch sebagai pengganti if else yg banyak
+console.log("contoh penggunaan Switch");
+const balonku = prompt("masukkan warna balon kamu!");
+
+switch (balonku) {
+  case "merah":
+    console.log("warna merah");
+    break;
+  case "hijau":
+    console.log("dooorrrrr");
+    break;
+  case "biru":
+    console.log("warna biru");
+    break;
+  default:
+    console.log("bukan balonku");
 }
