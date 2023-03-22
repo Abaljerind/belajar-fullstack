b5871493390f4796dfabca699261fce88aacc957
Abaljerind <abaljerinda@gmail.com>
2023-03-22 10:55:19 +0700

    add: contoh function scope

--- javascript/latihan1/function.js
+++ javascript/latihan1/function.js
@@ -21,15 +21,51 @@ console.log("contoh penggunaan function");
 // penjumlahan(2, 2);
 
 // contoh penggunaan return
-function jumlah(a = 0, b = 0) {
-  let total = a + b;
-  return total;
+// function jumlah(a = 0, b = 0) {
+//   let total = a + b;
+//   return total;
+// }
+
+// // contoh penggunaan return dengan kondisi yg memiliki return juga
+// function kurang(a, b) {
+//   if (typeof a !== "number" || typeof b !== "number") {
+//     return "bukan number, masukkan number";
+//   }
+//   return a - b;
+// }
+
+// contoh function scope
+console.log("contoh 1");
+
+let nama = "naruto";
+function namaAnggota() {
+  let nama = "sasuke";
+  console.log(nama);
 }
 
-// contoh penggunaan return dengan kondisi yg memiliki return juga
-function kurang(a, b) {
-  if (typeof a !== "number" || typeof b !== "number") {
-    return "bukan number, masukkan number";
-  }
-  return a - b;
+namaAnggota();
+console.log(nama);
+
+//
+console.log();
+console.log("contoh 2");
+
+let a = "tas";
+function barang() {
+  console.log(a);
 }
+
+barang();
+console.log(a);
+
+//
+console.log();
+console.log("contoh 3");
+
+function system() {
+  let os = "linux";
+  //   console.log(os);
+}
+
+system();
+console.log(os); // ini akan kosong karna tidak ada variable yg dibuat di luar function.
