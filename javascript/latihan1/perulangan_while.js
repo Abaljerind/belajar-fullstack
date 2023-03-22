2835c1f70b49f879728143f07bc3a3c9ebd1435c
Abaljerind <abaljerinda@gmail.com>
2023-03-22 07:05:30 +0700

    add: contoh while & for loop dengan break

--- javascript/latihan1/perulangan_while.js
+++ javascript/latihan1/perulangan_while.js
@@ -1,18 +1,34 @@
 console.log("perulangan dengan while");
 
 // contoh while loop sederhana
-let num = 0;
-while (num < 10) {
-  console.log(num);
-  num++;
+// let num = 0;
+// while (num < 10) {
+//   console.log(num);
+//   num++;
+// }
+
+// let password = "password";
+
+// let user = prompt("masukkan password");
+
+// while (user !== password) {
+//   user = prompt("masukkan password");
+// }
+
+// alert("selamat datang");
+
+// contoh while dan for dengan break
+
+// let input = prompt("coba ketik sesuatu");
+
+// while (true) {
+//   input = prompt("ketik lagi coba deh..");
+//   if (input.toLowerCase() === "stop") break;
+// }
+
+// alert("ok");
+
+for (i = 0; i < 10000; i++) {
+  console.log("nilai ke - " + i);
+  if (i === 100) break;
 }
-
-let password = "password";
-
-let user = prompt("masukkan password");
-
-while (user !== password) {
-  user = prompt("masukkan password");
-}
-
-alert("selamat datang");
