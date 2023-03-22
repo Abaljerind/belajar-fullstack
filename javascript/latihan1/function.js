c7df47521d40512227dc6568b287a717aafbfc5b
Abaljerind <abaljerinda@gmail.com>
2023-03-22 09:18:55 +0700

    add: contoh argument & parameter function

--- javascript/latihan1/function.js
+++ javascript/latihan1/function.js
@@ -1,5 +1,12 @@
 console.log("contoh penggunaan function");
 
-function lemparDadu() {
-  console.log("hasil lempar dadu " + Math.floor(Math.random() * 6 + 1));
+// function lemparDadu() {
+//   console.log("hasil lempar dadu " + Math.floor(Math.random() * 6 + 1));
+// }
+
+// contoh argument dan parameter pada function
+function salam(name) {
+  console.log(`Halo, selamat pagi ${name}`);
 }
+
+salam("naruto");
