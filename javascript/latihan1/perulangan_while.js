77c92627e95e212e38d6b166fa82cac244f7dc44
Abaljerind <abaljerinda@gmail.com>
2023-03-22 07:33:02 +0700

    add: contoh for..of loop biasa & array di dalam array

--- javascript/latihan1/perulangan_while.js
+++ javascript/latihan1/perulangan_while.js
@@ -28,7 +28,42 @@ console.log("perulangan dengan while");
 
 // alert("ok");
 
-for (i = 0; i < 10000; i++) {
-  console.log("nilai ke - " + i);
-  if (i === 100) break;
+// for (i = 0; i < 10000; i++) {
+//   console.log("nilai ke - " + i);
+//   if (i === 100) break;
+// }
+
+// contoh perulangan for..of
+
+let fruits = ["banana", "apple", "watermelon", "orange"];
+
+// kalau dgn for biasa maka akan seperti ini,
+// for (i = 0; i < fruits.length; i++) {
+//   console.log(`Buah ${fruits[i]}`);
+// }
+
+// dengan for..of
+// for (fruit of fruits) {
+//   console.log(`Buah ${fruit}`);
+// }
+
+// for..of dengan array di dalam array
+const rowStudent = [
+  ["olivia", "liam", "emma", "ussop"],
+  ["amelia", "oliver", "ava", "eli"],
+  ["sophia", "mateo", "isabela", "lucas"],
+];
+
+//   for (i = 0; i < rowStudent.length; i++) {
+//     let row = rowStudent[i];
+//     console.log("baris ke - " + i);
+//     for (j = 0; j < row.length; j++) {
+//       console.log("     " + row[j]);
+//     }
+//   }
+for (baris of rowStudent) {
+  console.log("ketua kelompok - " + baris[0]);
+  for (kolom of baris) {
+    console.log("     " + kolom);
+  }
 }
