038e45d6d81ec4b45d8812158ee3276d304d20ce
Abaljerind <abaljerinda@gmail.com>
2023-04-13 14:19:48 +0700

    create: folder keywordthis & isinya

---
+++ javascript dom/latihan5_keywordthis/app.js
@@ -0,0 +1,18 @@
+// generate random color dibuat
+const generateRandomColor = () => {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+
+  return `rgb(${red}, ${green}, ${blue})`;
+};
+
+// buat variable untuk menampung semua button yang ada
+const buttons = document.querySelectorAll("button");
+
+// buat perulangan for of dengan addEventListener di dalamnya agar semua button bisa men-generate warna nya masing masing
+for (let button of buttons) {
+  button.addEventListener("click", () => {
+    button.style.backgroundColor = generateRandomColor();
+  });
+}
