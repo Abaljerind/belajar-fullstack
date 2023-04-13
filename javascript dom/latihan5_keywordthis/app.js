28eb7d2e28e5a6b0ccb97607fa896d791d6fc2cb
Abaljerind <abaljerinda@gmail.com>
2023-04-13 14:37:18 +0700

    edit: bgcolor & color dgn keyword this

--- javascript dom/latihan5_keywordthis/app.js
+++ javascript dom/latihan5_keywordthis/app.js
@@ -11,18 +11,37 @@ const generateRandomColor = () => {
 const buttons = document.querySelectorAll("button");
 
 // buat perulangan for of dengan addEventListener di dalamnya agar semua button bisa men-generate warna nya masing masing
-for (let button of buttons) {
-  button.addEventListener("click", () => {
-    button.style.backgroundColor = generateRandomColor();
-    button.style.color = generateRandomColor();
-  });
-}
+// for (let button of buttons) {
+//   button.addEventListener("click", () => {
+//     button.style.backgroundColor = generateRandomColor();
+//     button.style.color = generateRandomColor();
+//   });
+// }
 
 const headings = document.querySelectorAll("h1");
 
+// for (let heading of headings) {
+//   heading.addEventListener("click", () => {
+//     heading.style.backgroundColor = generateRandomColor();
+//     heading.style.color = generateRandomColor();
+//   });
+// }
+
+// karena kita memiliki nilai style background color & color dari function yg sama di bagian for of dari headings dan buttons, maka kita bisa gunakan keyword "this" untuk mempersingkat penulisannya, dengan cara membuat function baru yang nantinya akan menggantikan anonymous function dibagian for of setelah "click"
+
+// setelah kedua for of diatas kita jadikan komentar, kita buat lagi yg baru seperti dibawah ini.
+
+// kita buat dulu function colorize untuk style backgroundColor dan colornya
+function colorize() {
+  this.style.backgroundColor = generateRandomColor();
+  this.style.color = generateRandomColor();
+}
+
+// lanjut kita buat for of yg baru
+for (let button of buttons) {
+  button.addEventListener("click", colorize);
+}
+
 for (let heading of headings) {
-  heading.addEventListener("click", () => {
-    heading.style.backgroundColor = generateRandomColor();
-    heading.style.color = generateRandomColor();
-  });
+  heading.addEventListener("click", colorize);
 }
