6a5e22e3471f3a85920f195501f00c42543c12f4
Abaljerind <abaljerinda@gmail.com>
2023-04-15 18:30:17 +0700

    add: eventListener untuk p2

--- javascript dom/latihan_scoreboard/app.js
+++ javascript dom/latihan_scoreboard/app.js
@@ -4,12 +4,18 @@ const p2Button = document.querySelector("#p2-button");
 const p1Display = document.querySelector("#p1-display");
 const p2Display = document.querySelector("#p2-display");
 
-// create var p1Score untuk menyimpan score yang dimasukkan
+// create var p1Score & p2Score untuk menyimpan score yang dimasukkan
 let p1Score = 0;
+let p2Score = 0;
 
 // add addEventListener di p1Button agar tiap kali button p1 di klik nilainya bertambah
 p1Button.addEventListener("click", function () {
   p1Score += 1;
   p1Display.textContent = p1Score;
   // textContent akan mengubah isi dari span dgn id p1Display sehingga text 0 di index.html akan berubah nilainya
+});
+
+p2Button.addEventListener("click", function () {
+  p2Score += 1;
+  p2Display.textContent = p2Score;
 });
