0bbd9452194858e7c030aa05c2a7f0822f93a1ed
Abaljerind <abaljerinda@gmail.com>
2023-05-05 21:50:03 +0700

    contoh handle error dgn async await

--- javascript asynchronous/latihan-callback-hell/app2.js
+++ javascript asynchronous/latihan-callback-hell/app2.js
@@ -1,3 +1,5 @@
+// Mengelola Kondisi Error Dengan Async Await
+
 // promise version
 const requestPromise = (url) => {
   return new Promise((resolve, reject) => {
@@ -11,3 +13,21 @@ const requestPromise = (url) => {
     }, delay);
   });
 };
+
+async function requestHandler() {
+  //   let result = await requestPromise("movie.com");
+  //   jika output yang muncul di console log adalah error, maka baris hello dibawah ini tidak akan dijalankan.
+  //   console.log("hello");
+  // untuk menghandle / menangani error diatas, kita bisa gunakan try and catch seperti dibawah ini.
+
+  try {
+    let result = await requestPromise("movie.com");
+    console.log(result);
+    // variable result akan mengambil nilai jika proses 'berhasil' dari bagian 'resolve(`Success: ${url} (${delay}ms)`);'
+  } catch (error) {
+    console.log("Pesan Error", error);
+    // dan error nya akan mengambil nilai jika proses 'gagal' dari bagian 'reject("Error: connection timeout");'
+  }
+}
+
+console.log(requestHandler());
