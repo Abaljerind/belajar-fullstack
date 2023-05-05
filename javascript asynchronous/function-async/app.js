b321b924f406874a9c814ca155b1586fac1189cd
Abaljerind <abaljerinda@gmail.com>
2023-05-05 20:50:14 +0700

    contoh throw error pada promise

--- javascript asynchronous/function-async/app.js
+++ javascript asynchronous/function-async/app.js
@@ -5,13 +5,26 @@
 
 // -------------------------------------------------------------
 // contoh jika function nya punya data (catatan async part b.)
-const hello = async () => {
-  return "hello world!";
-};
+// const hello = async () => {
+//   return "hello world!";
+// };
 
-// memanggil function hello()
-hello().then((res) => {
-  console.log("response", res);
-});
+// // memanggil function hello()
+// hello().then((res) => {
+//   console.log("response", res);
+// });
 
 // -------------------------------------------------------------
+// contoh jika function nya punya object throw error (catatan async part c.)
+const hello = async () => {
+  throw "Error, ada kesalahan di baris kode anda";
+};
+
+hello()
+  .then((res) => {
+    console.log("response", res);
+  })
+  //   agar kita bisa jelas mendapatkan kejelasan dari pesan error nya, maka bisa ditambahkan .catch seperti dibawah.
+  .catch((err) => {
+    console.log("error", err);
+  });
