59f5819cbdbd07e2c84489c6b3454c3d1c024cc5
Abaljerind <abaljerinda@gmail.com>
2023-05-05 15:32:22 +0700

    add  perbedaan promise & callback hell

--- javascript asynchronous/latihan-callback-hell/app.js
+++ javascript asynchronous/latihan-callback-hell/app.js
@@ -1,4 +1,4 @@
-// callback version, alasan kenapa tidak disarankan menggunakannya
+// callback version
 const requestCallback = (url, success, failure) => {
   const delay = Math.floor(Math.random() * 4500) + 500;
   setTimeout(() => {
@@ -105,42 +105,75 @@ const requestPromise = (url) => {
 
 // promise juga sebenarnya masih bisa mengalami callback hell seperti requestCallback sebelumnya, seperti berikut kode nya kalau pada promise.
 
+// requestPromise("movie.com")
+//   .then((response) => {
+//     console.log("success", response);
+
+//     requestPromise("movie.com")
+//       .then((response) => {
+//         console.log("success", response);
+
+//         requestPromise("movie.com")
+//           .then((response) => {
+//             console.log("success", response);
+
+//             requestPromise("movie.com")
+//               .then((response) => {
+//                 console.log("success", response);
+
+//                 requestPromise("movie.com")
+//                   .then((response) => {
+//                     console.log("success", response);
+//                   })
+//                   .catch((error) => {
+//                     console.log("error", error);
+//                   });
+//               })
+//               .catch((error) => {
+//                 console.log("error", error);
+//               });
+//           })
+//           .catch((error) => {
+//             console.log("error", error);
+//           });
+//       })
+//       .catch((error) => {
+//         console.log("error", error);
+//       });
+//   })
+//   .catch((error) => {
+//     console.log("error", error);
+//   });
+
+//
+//
+//
+// cara yang benar menggunakan fungsi promise
+// requestPromise("movie.com")
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// perbedaan dengan requestCallback adalah:
+// 1. requestPromise dapat digunakan untuk bagian yang berhasil di halaman (page) berbeda seperti dibawah ini, dengan tampilan yang lebih mudah dibaca.
+// 2. bagian "catch nya kita hanya butuh 1 saja".
 requestPromise("movie.com")
-  .then((response) => {
-    console.log("success", response);
-
-    requestPromise("movie.com")
-      .then((response) => {
-        console.log("success", response);
-
-        requestPromise("movie.com")
-          .then((response) => {
-            console.log("success", response);
-
-            requestPromise("movie.com")
-              .then((response) => {
-                console.log("success", response);
-
-                requestPromise("movie.com")
-                  .then((response) => {
-                    console.log("success", response);
-                  })
-                  .catch((error) => {
-                    console.log("error", error);
-                  });
-              })
-              .catch((error) => {
-                console.log("error", error);
-              });
-          })
-          .catch((error) => {
-            console.log("error", error);
-          });
-      })
-      .catch((error) => {
-        console.log("error", error);
-      });
+  .then((result) => {
+    console.log("page 1");
+    console.log(result);
+    return requestPromise("movie.com");
   })
-  .catch((error) => {
-    console.log("error", error);
+  .then(() => {
+    console.log("page 2");
+    return requestPromise("movie.com");
+  })
+  .then(() => {
+    console.log("page 3");
+    return requestPromise("movie.com");
+  })
+  .catch((err) => {
+    console.log(err);
   });
