2a61b027d8a5767078680719389ee8bf57b8b5aa
Abaljerind <abaljerinda@gmail.com>
2023-06-06 17:24:28 +0700

    contoh menggunakan data object terpisah

--- vue/chapter-1-installasi/app.js
+++ vue/chapter-1-installasi/app.js
@@ -1,7 +1,20 @@
 // salah satu fitur unggulan dari vue js, bagaimana mengelola virtual dom
+// var app = new Vue({
+//   el: "#app", // el: artinya elemen yang punya id "app".
+//   data: {
+//     name: "Naruto Uzumaki",
+//   },
+// });
+
+// contoh data lagi
+const narutoShippuden = {
+  name: "naruto uzumaki shippuden",
+  deskripsi: "bercerita tentang naruto uzumaki, seorang ninja asal konoha yang ingin menjadi hokage",
+  price: "25000",
+  image: "https://upload.wikimedia.org/wikipedia/id/9/94/NarutoCoverTankobon1.jpg",
+};
+
 var app = new Vue({
-  el: "#app", // el: artinya elemen yang punya id "app".
-  data: {
-    name: "Naruto Uzumaki",
-  },
+  el: "#app",
+  data: narutoShippuden,
 });
