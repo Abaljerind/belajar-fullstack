9f4c7e8468a16fc1950156a180f02f987c408a4d
Abaljerind <abaljerinda@gmail.com>
2023-06-06 15:14:31 +0700

    add notes, app.js, index.html

---
+++ vue/chapter-1-installasi/app.js
@@ -0,0 +1,7 @@
+// salah satu fitur unggulan dari vue js, bagaimana mengelola virtual dom
+var app = new Vue({
+  el: "#app", // el: artinya elemen yang punya id "app".
+  data: {
+    name: "Naruto Uzumaki",
+  },
+});
