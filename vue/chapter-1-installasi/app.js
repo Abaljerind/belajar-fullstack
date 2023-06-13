c40703cca9b6018e3ddc989d74212e8c1f100c81
Abaljerind <abaljerinda@gmail.com>
2023-06-13 21:33:46 +0700

    contoh event + methods

--- vue/chapter-1-installasi/app.js
+++ vue/chapter-1-installasi/app.js
@@ -57,6 +57,7 @@ var app = new Vue({
   data: {
     maximum: 50,
     products: null,
+    cart: [],
   },
   mounted: function () {
     fetch("https://hplussport.com/api/products/order/price")
@@ -64,5 +65,10 @@ var app = new Vue({
       .then((data) => {
         this.products = data;
       });
+  },
+  methods: {
+    addItem: function (product) {
+      this.cart.push(product);
+    },
   },
 });
