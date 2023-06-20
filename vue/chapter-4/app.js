dad26df14b6392f0a6bb8ab47e77abe3aad6155f
Abaljerind <abaljerinda@gmail.com>
2023-06-20 11:22:56 +0700

    contoh computed & methods

--- vue/chapter-4/app.js
+++ vue/chapter-4/app.js
@@ -4,13 +4,28 @@ var app = new Vue({
     slugText: "The Quick #(*!&@ Brown 29",
   },
   computed: {
+    // now: function () {
+    //   var date = new Date();
+    //   return String(date.getHours()) + String(date.getMinutes()) + String(date.getSeconds());
+    // },
     slugetize: function () {
-      return this.slugText
-        .toLowerCase() // isi slugText akan diubah jadi lowercase
-        .replace(/[^\w ]+/g, "") // setiap simbol akan dihapus dengan string kosong. baris kode didalam tanda kurung menggunakan regular expression.
-        .replace(/ +/g, "-"); // mengubah setiap whitespace menjadi dash / -
+      return (
+        this.slugText
+          .toLowerCase() // isi slugText akan diubah jadi lowercase
+          .replace(/[^\w ]+/g, "") // setiap simbol akan dihapus dengan string kosong. baris kode didalam tanda kurung menggunakan regular expression.
+          .replace(/ +/g, "-") +
+        "-" +
+        this.now()
+      ); // mengubah setiap whitespace menjadi dash / - serta menambah dash dan juga function now dari property methods.
+    },
+  },
+  methods: {
+    now: function () {
+      var date = new Date();
+      return String(date.getHours()) + String(date.getMinutes()) + String(date.getSeconds());
     },
   },
 });
 
 // output dari function slugetize: the-quick-brown-29
+// output terbaru dari slugetize jadinya: the-quick-brown-29-11453, dimana 11 adalah jam, 4 adalah menit, dan 53 adalah detik.
