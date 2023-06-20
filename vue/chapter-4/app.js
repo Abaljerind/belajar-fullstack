569e4dad1a1f6188c183edbda85113371b2ef771
Abaljerind <abaljerinda@gmail.com>
2023-06-20 10:59:48 +0700

    contoh computed & function slug

--- vue/chapter-4/app.js
+++ vue/chapter-4/app.js
@@ -3,4 +3,14 @@ var app = new Vue({
   data: {
     slugText: "The Quick #(*!&@ Brown 29",
   },
+  computed: {
+    slugetize: function () {
+      return this.slugText
+        .toLowerCase() // isi slugText akan diubah jadi lowercase
+        .replace(/[^\w ]+/g, "") // setiap simbol akan dihapus dengan string kosong. baris kode didalam tanda kurung menggunakan regular expression.
+        .replace(/ +/g, "-"); // mengubah setiap whitespace menjadi dash / -
+    },
+  },
 });
+
+// output dari function slugetize: the-quick-brown-29
