e59fb30b1d2243afcf8490947d347d4e66fee813
Abaljerind <abaljerinda@gmail.com>
2023-06-21 20:42:16 +0700

    edit addItem u/ category data array

--- vue/chapter-1-installasi/app.js
+++ vue/chapter-1-installasi/app.js
@@ -102,7 +102,24 @@ var app = new Vue({
       }, delay);
     },
     addItem: function (product) {
-      this.cart.push(product);
+      // this.cart.push(product);
+
+      // pengkategorian data array
+      var productIndex;
+      var productExist = this.cart.filter(function (item, index) {
+        if (item.product.id == Number(product.id)) {
+          productIndex = index;
+          return true;
+        } else {
+          return false;
+        }
+      });
+
+      if (productExist.length) {
+        this.cart[productIndex].qty++;
+      } else {
+        this.cart.push({ product: product, qty: 1 });
+      }
     },
   },
 });
