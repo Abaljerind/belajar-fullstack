1c47f44ab2c2825e914a26c637d5fc2449959a60
Abaljerind <abaljerinda@gmail.com>
2023-10-28 21:27:04 +0700

    add uppercase() & slice() to make capitalize

--- nodejs/example/part3-node-web-server/demo_querystring-2.js
+++ nodejs/example/part3-node-web-server/demo_querystring-2.js
@@ -5,11 +5,21 @@ http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
     let query = url.parse(req.url, true).query;
-
-    if (query.fullName) {
-      res.end("halo " + query.fullName + ". Saya Jeri." + "\n");
+    if (query.firstName || query.lastName) {
+      res.end(
+        "halo " +
+          query.firstName.charAt(0).toUpperCase() +
+          query.firstName.slice(1) +
+          " " +
+          query.lastName.charAt(0).toUpperCase() +
+          query.lastName.slice(1) +
+          ". Saya Jeri." +
+          "\n"
+      );
     } else {
       res.end("selamat datang!");
     }
   })
   .listen(8080);
+
+// untuk akses gunakan url ini: http://localhost:8080/status?firstName=naruto&lastName=uzumaki
