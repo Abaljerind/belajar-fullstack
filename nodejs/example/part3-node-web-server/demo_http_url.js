b4627c5322d00c89d0a3e2cfad8a9d076d2c006b
Abaljerind <abaljerinda@gmail.com>
2023-10-28 21:30:15 +0700

    add comment to visit url

--- nodejs/example/part3-node-web-server/demo_http_url.js
+++ nodejs/example/part3-node-web-server/demo_http_url.js
@@ -1,6 +1,11 @@
-const http = require('http');
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write(req.url);
-	res.end();
-}).listen(8080);
+const http = require("http");
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(req.url);
+    res.end();
+  })
+  .listen(8080);
+
+// untuk akses gunakan url ini: http://localhost:8080/summer
+// bagian summer nya bisa diganti dengan yang lain.
