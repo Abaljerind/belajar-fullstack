0c503d2f01a7974fa89fc991e44fc7c0fc13e409
Abaljerind <abaljerinda@gmail.com>
2023-10-30 21:49:58 +0700

    create & add content to both files

---
+++ nodejs/example/part4-read-files/demo_readfile.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const fs = require("fs");
+
+http
+  .createServer(function (res, req) {
+    // membuka sebuah file diserver dan mengembalikkan isinya
+    fs.readFile("demofile1.html", function (err, data) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);
