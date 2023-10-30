05b8c385a31258eb2441231f8a64b20d59a3db44
Abaljerind <abaljerinda@gmail.com>
2023-10-30 22:06:30 +0700

    fix this file

--- nodejs/example/part4-read-files/demo_readfile.js
+++ nodejs/example/part4-read-files/demo_readfile.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 
 http
-  .createServer(function (res, req) {
+  .createServer(function (req, res) {
     // membuka sebuah file diserver dan mengembalikkan isinya
     fs.readFile("demofile1.html", function (err, data) {
       res.writeHead(200, { "Content-Type": "text/html" });
