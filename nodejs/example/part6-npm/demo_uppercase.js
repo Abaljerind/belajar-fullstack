4950222afe91939f20f806ecbf1e7ebadb5dfa1f
Abaljerind <abaljerinda@gmail.com>
2023-10-31 21:03:48 +0700

    add notes error warning

--- nodejs/example/part6-npm/demo_uppercase.js
+++ nodejs/example/part6-npm/demo_uppercase.js
@@ -1,10 +1,12 @@
+import { upperCase, localeUpperCase } from "upper-case";
 const http = require("http");
-const uc = require("upper-case");
 
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(uc.upperCase("halo dunia fana!"));
+    res.write(upperCase("halo dunia fana!"));
     res.end();
   })
   .listen(8080);
+
+// error, upper-case module nya sudah deprecated, contoh penggunaannya kurang jelas.
