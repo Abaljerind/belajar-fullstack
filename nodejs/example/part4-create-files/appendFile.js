c92138f0b6cdd1c4e938b41914a402e17197184c
Abaljerind <abaljerinda@gmail.com>
2023-10-31 10:21:10 +0700

    update file using appendFile()

--- nodejs/example/part4-create-files/appendFile.js
+++ nodejs/example/part4-create-files/appendFile.js
@@ -1,6 +1,13 @@
 const fs = require("fs");
 
-fs.appendFile("mynewfile.txt", "Hello Content! How are you?", function (err) {
+// membuat file baru dan isinya
+// fs.appendFile("mynewfile.txt", "Hello Content! How are you?", function (err) {
+//   if (err) throw err;
+//   console.log("Saved");
+// });
+
+// menambahkan isi text baru kedalam file mynewfile.txt diakhir filenya
+fs.appendFile("mynewfile.txt", "\nThis is my new text!", function (err) {
   if (err) throw err;
-  console.log("Saved");
+  console.log("Updated!");
 });
