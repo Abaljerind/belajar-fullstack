a95522ac74d077fb63bd8ebfd255325036afae83
Abaljerind <abaljerinda@gmail.com>
2023-10-31 10:23:31 +0700

    update file using writeFile()

--- nodejs/example/part4-create-files/writeFile.js
+++ nodejs/example/part4-create-files/writeFile.js
@@ -1,6 +1,13 @@
 const fs = require("fs");
 
-fs.writeFile("mynewfile3.txt", "Hello writeFile.js!!", function (err) {
+// kode membuat file baru dan isinya
+// fs.writeFile("mynewfile3.txt", "Hello writeFile.js!!", function (err) {
+//   if (err) throw err;
+//   console.log("Saved!");
+// });
+
+// kode untuk update isi file, untuk writeFile() ini isinya akan digantikan.
+fs.writeFile("mynewfile3.txt", "This is my new text! XD", function (err) {
   if (err) throw err;
-  console.log("Saved!");
+  console.log("updated!");
 });
