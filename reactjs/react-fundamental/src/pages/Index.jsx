dc2dcbc71b1a88e0f9b8710f64c602ad2ca484ae
Abaljerind <abaljerinda@gmail.com>
2025-01-04 14:52:56 +0700

    Import posts and Article
    mapping posts with map and return value to Article
    Article accept props

--- reactjs/react-fundamental/src/pages/Index.jsx
+++ reactjs/react-fundamental/src/pages/Index.jsx
@@ -1,7 +1,13 @@
+import posts from "../posts.json";
+import Article from "../components/Article";
+
 export default function HomePage() {
   return (
     <>
       <h1>Simple Blog</h1>
+      {posts.map((blog) => {
+        return <Article title={blog.title} tags={blog.tags} date={blog.date} />;
+      })}
     </>
   );
 }
