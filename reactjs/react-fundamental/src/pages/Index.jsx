102b14b643fd61a6a90e82a093de6e1197f2556f
AbalJerind <abaljerinda@gmail.com>
2025-01-07 15:20:42 +0700

    Create new useState for counting how many posts found
    Send total posts found to Search component

--- reactjs/react-fundamental/src/pages/Index.jsx
+++ reactjs/react-fundamental/src/pages/Index.jsx
@@ -5,18 +5,20 @@ import Search from "../components/Search";
 
 function HomePage() {
   const [posts, setPosts] = useState(postsData);
+  const [totalPosts, setTotalPosts] = useState(0);
 
   const onSearchChange = (value) => {
     const filteredPosts = postsData.filter((post) =>
       post.title.includes(value)
     );
     setPosts(filteredPosts);
+    setTotalPosts(filteredPosts.length);
   };
 
   return (
     <>
       <h1>Simple Blog</h1>
-      <Search onSearchChange={onSearchChange} />
+      <Search onSearchChange={onSearchChange} totalPosts={totalPosts} />
       {posts.map((props, index) => {
         return <Article {...props} key={index} />;
       })}
