46133a09170f75dbe14562a871c39772bc71cd1c
Abaljerind <abaljerinda@gmail.com>
2025-01-08 09:36:55 +0700

    Add button with onClick attr
    Edit input attr onChange to setSearch
    Edit onSearchChange method

--- reactjs/react-fundamental/src/components/Search.jsx
+++ reactjs/react-fundamental/src/components/Search.jsx
@@ -3,15 +3,15 @@ import { useState } from "react";
 function Search(props) {
   const [search, setSearch] = useState("");
 
-  const onSearchChange = (e) => {
-    setSearch(e.target.value);
-    props.onSearchChange(e.target.value);
+  const onSearchChange = () => {
+    props.onSearchChange(search);
   };
 
   return (
     <>
       <div>
-        Cari Artikel : <input type="text" onChange={onSearchChange} />
+        Cari Artikel : <input type="text" onChange={(e) => setSearch(e.target.value)} />
+        <button onClick={onSearchChange}>Cari</button>
       </div>
       <small>
         Ditemukan {props.totalPosts} data dengan kata {search}
