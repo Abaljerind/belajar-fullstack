50654a9a3133eac68eb2ef4ec2753880a9c9705d
Abaljerind <abaljerinda@gmail.com>
2025-01-09 21:19:41 +0700

    Add new route for About page

--- reactjs/react-fundamental/src/routers/index.jsx
+++ reactjs/react-fundamental/src/routers/index.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/";
+import About from "../pages/About";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/about",
+    element: <About />,
   },
 ]);
