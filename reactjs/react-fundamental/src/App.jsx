2a4af2ce6d51b46df803a7c30fd4833a860b198e
Abaljerind <abaljerinda@gmail.com>
2025-01-09 16:56:52 +0700

    Import GlobalContext from index.js
    Create new object and add GlobalContext as component with value of the new object
    Put HomePage component inside GlobalContext component

--- reactjs/react-fundamental/src/App.jsx
+++ reactjs/react-fundamental/src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
 import HomePage from "./pages/Index";
+import { GlobalContext } from "./context";
 
 function App() {
+  const user = {
+    username: "Naruto",
+  };
+
   return (
     <>
       <div className="App">
-        <HomePage />
+        <GlobalContext.Provider value={user}>
+          <HomePage />
+        </GlobalContext.Provider>
       </div>
     </>
   );
