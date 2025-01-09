65f7b684e3cd76fef7a3f2501f3f6115aa01caf1
Abaljerind <abaljerinda@gmail.com>
2025-01-09 17:09:06 +0700

    Import useContext and GlobalContext
    Comment some codes
    Create new variable with useContext and GlobalContext as the value
    Access the value from GlobalContext via the variable

--- reactjs/react-fundamental/src/components/Article.jsx
+++ reactjs/react-fundamental/src/components/Article.jsx
@@ -1,22 +1,29 @@
+import { useContext } from "react";
+import { GlobalContext } from "../context";
+
 function ArticleStatus({ isNew }) {
   return <i style={{ color: "red" }}>{isNew && "New Article -- "}</i>; // conditional rendering using component
 }
 
 // conditional rendering not using props
-function NewArticle() {
-  return <i style={{ color: "red" }}>New Article -- </i>;
-}
+// function NewArticle() {
+//   return <i style={{ color: "red" }}>New Article -- </i>;
+// }
 
 function Article(props) {
+  const user = useContext(GlobalContext);
   return (
     <>
       <h3>{props.title}</h3>
       <small>
         <ArticleStatus isNew={props.isNew} />
-        {props.isNew && <NewArticle />}
+        {/* {props.isNew && <NewArticle />} */}
         <strong>Tags:</strong> {props.tags.join(", ")}, <strong>Date:</strong>
         {props.date}
       </small>
+      <div>
+        <small>ditulis oleh {user.username}</small>
+      </div>
     </>
   );
 }
