02f6c4f811e1b27b20bbcccdb3da174843548bff
Abaljerind <abaljerinda@gmail.com>
2025-01-10 09:25:15 +0700

    Add new routing for path and element of Blog

--- reactjs/react-fundamental/src/routers/index.jsx
+++ reactjs/react-fundamental/src/routers/index.jsx
@@ -3,6 +3,7 @@ import RootLayout from "../layouts/RootLayout";
 
 import HomePage from "../pages/";
 import About from "../pages/About";
+import Blog from "../pages/blogs";
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <HomePage />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
       },
       {
         path: "/about",
