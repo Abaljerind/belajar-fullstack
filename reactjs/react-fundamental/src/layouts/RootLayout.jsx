ce9686f4397df1bf4d9308dc0b835e616772d0f0
Abaljerind <abaljerinda@gmail.com>
2025-01-10 08:56:25 +0700

    Add outlet for display body of page & add style to Link about

--- reactjs/react-fundamental/src/layouts/RootLayout.jsx
+++ reactjs/react-fundamental/src/layouts/RootLayout.jsx
@@ -3,7 +3,12 @@ import { Link, Outlet } from "react-router-dom";
 function RootLayout() {
   return (
     <>
-      <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      <Link to="/">Home</Link> |{" "}
+      <Link to="/about" style={{ color: "teal" }}>
+        About
+      </Link>
+      <br />
+      <Outlet />
     </>
   );
 }
