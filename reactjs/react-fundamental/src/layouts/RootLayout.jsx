7654abb58d124ec86a9bd2292da009665998e322
Abaljerind <abaljerinda@gmail.com>
2025-01-10 09:23:08 +0700

    Add Link to /blog and give it a style color

--- reactjs/react-fundamental/src/layouts/RootLayout.jsx
+++ reactjs/react-fundamental/src/layouts/RootLayout.jsx
@@ -4,6 +4,10 @@ function RootLayout() {
   return (
     <>
       <Link to="/">Home</Link> |{" "}
+      <Link to="/blog" style={{ color: "whitesmoke" }}>
+        Blog
+      </Link>{" "}
+      |{" "}
       <Link to="/about" style={{ color: "teal" }}>
         About
       </Link>
