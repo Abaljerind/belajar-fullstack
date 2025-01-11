b09e7c3e652d144e3c0868b38077efd8158b5dbb
Abaljerind <abaljerinda@gmail.com>
2025-01-11 09:39:38 +0700

    Add some component
    Add Header, Menu and Footer with each different value

--- reactjs/food-menu/src/App.jsx
+++ reactjs/food-menu/src/App.jsx
@@ -1,19 +1,37 @@
 function App() {
   return (
     <>
-      <h1>Hello, React!</h1>
-      <Food />
-      <Food />
-      <Food />
-      <Food />
+      <Header />
+      <Menu />
+      <Footer />
     </>
   );
+}
+
+function Header() {
+  return <h1>Warteg Mang Udin</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Menu Kita</h2>
+      <Food />
+      <Food />
+      <Food />
+      <Food />
+    </div>
+  );
+}
+
+function Footer() {
+  return <footer>{new Date().getFullYear()} Warteg Mang Udin</footer>;
 }
 
 function Food() {
   return (
     <div>
-      <img src="../food/soto-betawi.jpg" alt="Soto Betawi" />
+      <img src="../food/soto-betawi.jpg" alt="Soto Betawi" width={100} height={70} />
       <h2>Soto Betawi</h2>
       <p>Soto Betawi dari Jakarta</p>
     </div>
