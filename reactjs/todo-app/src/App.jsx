c346e780d1e8c23be46fa514fa77a4a70f1884ac
Abaljerind <abaljerinda@gmail.com>
2025-01-13 20:25:09 +0700

    Create 2 useState for saving notes and inputNotes
    Add some HTML elements to display the notes from state

--- reactjs/todo-app/src/App.jsx
+++ reactjs/todo-app/src/App.jsx
@@ -1,8 +1,32 @@
+import { useState } from "react";
+
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      text: "hello",
+    },
+  ]);
+  const [inputNotes, setInputNotes] = useState("");
+
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <div className="App">
+      <h1>Notes</h1>
+      <div className="note-input">
+        <input type="text" name="" id="" placeholder="Add a note" />
+        <button>Add</button>
+      </div>
+      <ul className="note-list">
+        {notes.map((note) => {
+          return (
+            <>
+              <li key={note.id}>{note.text}</li>
+              <button>Delete</button>
+            </>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
 
