818314f830e1ba66fcf79bb3732699dd9239f290
Abaljerind <abaljerinda@gmail.com>
2025-01-13 15:33:13 +0700

    Set up gocheck app
    Create 4 component: Logo, Form, CheckList and Stats
    Add static text to each component

--- reactjs/gocheck/src/App.jsx
+++ reactjs/gocheck/src/App.jsx
@@ -1,10 +1,44 @@
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className="app"></div>
+      <div className="app">
+        <Logo />
+        <Form />
+        <CheckList />
+        <Stats />
+      </div>
     </>
+  );
+}
+
+function Logo() {
+  return <span className="logo">📃 GoCheck ✅</span>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>Tambahkan yang mau kamu catat 🫡</h3>
+    </div>
+  );
+}
+
+function CheckList() {
+  return (
+    <div className="list">
+      <ul>
+        <li>Makan</li>
+        <li>Tidur</li>
+      </ul>
+    </div>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <span>📃 Kamu punya x catatan dan baru x yang di selesai (x%) ✅</span>
+    </footer>
   );
 }
 
