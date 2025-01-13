ce4d0bd11b12c841df228100b934db94b1715837
Abaljerind <abaljerinda@gmail.com>
2025-01-13 20:47:41 +0700

    Create 3 event handler
    Create handleInputChange for input, handleSubmit for form and handleDeleteNote or button
    Edit div to be form
    Delete index.css

--- reactjs/todo-app/src/App.jsx
+++ reactjs/todo-app/src/App.jsx
@@ -9,19 +9,41 @@ function App() {
   ]);
   const [inputNotes, setInputNotes] = useState("");
 
+  function handleInputChange(e) {
+    setInputNotes(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (inputNotes.trim() !== "") {
+      const newNotes = {
+        id: new Date().getTime(),
+        text: inputNotes,
+      };
+
+      setNotes([...notes, newNotes]);
+      setInputNotes("");
+    }
+  }
+
+  function handleDeleteNote(id) {
+    const updatedNotes = notes.filter((note) => note.id !== id);
+    setNotes(updatedNotes);
+  }
+
   return (
     <div className="App">
       <h1>Notes</h1>
-      <div className="note-input">
-        <input type="text" name="" id="" placeholder="Add a note" />
+      <form className="note-input" onSubmit={handleSubmit}>
+        <input type="text" name="" id="" placeholder="Add a note" value={inputNotes} onChange={handleInputChange} />
         <button>Add</button>
-      </div>
+      </form>
       <ul className="note-list">
         {notes.map((note) => {
           return (
             <>
               <li key={note.id}>{note.text}</li>
-              <button>Delete</button>
+              <button onClick={() => handleDeleteNote(note.id)}>Delete</button>
             </>
           );
         })}
