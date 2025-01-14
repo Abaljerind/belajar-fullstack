8fe34a6689a5fb8e16628befa1b12d2e769bebb3
Abaljerind <abaljerinda@gmail.com>
2025-01-14 15:30:50 +0700

    Import Accordion component with props data value from faqs variable
    Create faqs array with 3 object data type consist of question and answer as property

--- reactjs/accordion-faq/src/App.jsx
+++ reactjs/accordion-faq/src/App.jsx
@@ -1,5 +1,27 @@
+import Accordion from "./components/Accordion";
+
+const faqs = [
+  {
+    question: "Apakah produk ini memiliki jaminan?",
+    answer: "Ya, kami memberikan jaminan garansi selama 1 tahun untuk produk kami.",
+  },
+  {
+    question: "Berapa lama waktu pengiriman rata-rata?",
+    answer: "Waktu pengiriman rata-rata adalah 3-5 hari kerja, tergantung lokasi pengiriman.",
+  },
+  {
+    question: "Bagaimana cara mengembalikan barang yang rusak?",
+    answer:
+      "Jika barang Anda rusak, hubungi layanan pelanggan kami untuk panduan mengenai proses pengembalian dan penggantian.",
+  },
+];
+
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
 }
 
 export default App;
