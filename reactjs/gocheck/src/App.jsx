8b97803d8eb4980339ccc7c3ed27e4f10a68d92a
Abaljerind <abaljerinda@gmail.com>
2025-01-14 11:58:23 +0700

    Add new div.actions for sorting listItems inside CheckList
    Add select with attr value and onChange
    Add 3 option with values input, title and status to sort listItems
    Create state for sortBy and setSortBy with default value input
    Create function sortItems() and inside there's switch case to sort listItems based on sortBy state
    Create new variable, sortedItems to save sortItems()
    Change props.listItems with sortedItems to make sure the data display are sorted based on select

--- reactjs/gocheck/src/App.jsx
+++ reactjs/gocheck/src/App.jsx
@@ -59,13 +59,36 @@ function Form(props) {
 }
 
 function CheckList(props) {
+  const [sortBy, setSortBy] = useState("input");
+
+  function sortItems() {
+    switch (sortBy) {
+      case "title":
+        return props.listItems.slice().sort((a, b) => a.title.localeCompare(b.title));
+      case "status":
+        return props.listItems.slice().sort((a, b) => Number(a.done) - Number(b.done));
+      case "input":
+      default:
+        return props.listItems;
+    }
+  }
+
+  const sortedItems = sortItems();
+
   return (
     <div className="list">
       <ul>
-        {props.listItems.map((item) => {
+        {sortedItems.map((item) => {
           return <Item item={item} key={item.id} onDeleteItem={props.onDeleteItem} onToggleItem={props.onToggleItem} />;
         })}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Urutkan berdasarkan input</option>
+          <option value="title">Urutkan berdasarkan judul</option>
+          <option value="status">Urutkan berdasarkan status</option>
+        </select>
+      </div>
     </div>
   );
 }
