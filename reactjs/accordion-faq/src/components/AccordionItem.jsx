305b39cf633e8c7da8e563ba04248345da2dff07
Abaljerind <abaljerinda@gmail.com>
2025-01-14 15:33:32 +0700

    Create AccordionItem and receive 3 props: number, question and answer
    Return div.item with 4 p elements
    Consist of .number, .title, .icon and .content-box

---
+++ reactjs/accordion-faq/src/components/AccordionItem.jsx
@@ -0,0 +1,12 @@
+function AccordionItem({ number, question, answer }) {
+  return (
+    <div className="item">
+      <p className="number">{number}</p>
+      <p className="title">{question}</p>
+      <p className="icon">+</p>
+      <p className="content-box">{answer}</p>
+    </div>
+  );
+}
+
+export default AccordionItem;
