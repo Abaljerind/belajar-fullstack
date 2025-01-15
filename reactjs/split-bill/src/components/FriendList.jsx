46ac3d21956c423e1ed0b3ebf192ec3e3ddd9d55
Abaljerind <abaljerinda@gmail.com>
2025-01-15 14:13:23 +0700

    Receive selectedFriend as props and send it to Friend component

--- reactjs/split-bill/src/components/FriendList.jsx
+++ reactjs/split-bill/src/components/FriendList.jsx
@@ -1,10 +1,12 @@
 import Friend from "./Friend";
 
-export default function FriendList({ friends, onSelectedFriend }) {
+export default function FriendList({ friends, onSelectedFriend, selectedFriend }) {
   return (
     <ul>
       {friends.map((friend) => {
-        return <Friend friend={friend} key={friend.id} onSelectedFriend={onSelectedFriend} />;
+        return (
+          <Friend friend={friend} key={friend.id} onSelectedFriend={onSelectedFriend} selectedFriend={selectedFriend} />
+        );
       })}
     </ul>
   );
