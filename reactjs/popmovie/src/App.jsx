30dd4e6fb149ee74b9ce85de0ccc561c7cadc08f
Abaljerind <abaljerinda@gmail.com>
2025-01-15 21:34:23 +0700

    Divide contain of nav into 3 different components
    Create Logo, Search and NumResults component to be contain of nav
    Move query state into Search component
    Send movies state from App to NumResults via props

--- reactjs/popmovie/src/App.jsx
+++ reactjs/popmovie/src/App.jsx
@@ -49,8 +49,38 @@ const tempWatchedData = [
 
 const average = (arr) => arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-export default function App() {
+function Logo() {
+  return (
+    <div className="logo">
+      <span role="img">🎫</span>
+      <h1>Movie</h1>
+    </div>
+  );
+}
+
+function Search() {
   const [query, setQuery] = useState("");
+
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+  );
+}
+
+function NumResults({ movies }) {
+  return (
+    <p className="num-results">
+      Found <strong>{movies.length}</strong> results
+    </p>
+  );
+}
+
+export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
   const [isOpen1, setIsOpen1] = useState(true);
@@ -63,20 +93,9 @@ export default function App() {
   return (
     <>
       <nav className="nav-bar">
-        <div className="logo">
-          <span role="img">🎫</span>
-          <h1>Movie</h1>
-        </div>
-        <input
-          className="search"
-          type="text"
-          placeholder="Search movies..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <p className="num-results">
-          Found <strong>{movies.length}</strong> results
-        </p>
+        <Logo />
+        <Search />
+        <NumResults movies={movies} />
       </nav>
 
       <main className="main">
