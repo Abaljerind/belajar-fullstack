8b4d3e9aa65a492da9647d91be208891a448eb8b
Abaljerind <abaljerinda@gmail.com>
2025-01-15 11:35:27 +0700

    Receive onAddFriend as props and create 2 state for name & image
    Create handleSubmit(e) to check name and image value
    Create variable id and newFriend for creating a new data of friend
    Invoke onAddFriend(newFriend) to send data to App.jsx with value from FormAddFriend
    Re-setName with empty string
    Add onSubmit with value handleSubmit inside form tag
    Add value from state for name and image inside input tag
    Add onChange to set both value name and image from input tag

--- reactjs/split-bill/src/components/FormAddFriend.jsx
+++ reactjs/split-bill/src/components/FormAddFriend.jsx
@@ -1,11 +1,33 @@
-export default function FormAddFriend() {
+import { useState } from "react";
+
+export default function FormAddFriend({ onAddFriend }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) return alert("Masukkan nama teman anda");
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+
+    onAddFriend(newFriend);
+    setName("");
+  }
+
   return (
-    <form action="" className="form-add-friend">
+    <form action="" className="form-add-friend" onSubmit={handleSubmit}>
       <label htmlFor="">🙎Nama</label>
-      <input type="text" />
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
 
       <label htmlFor="">📸Gambar</label>
-      <input type="text" />
+      <input type="text" value={image} onChange={(e) => setImage(e.target.value)} />
 
       <button className="button">Tambah</button>
     </form>
