d5ae3e8daaeb541cd82f3027e9d0088c4301b604
Abaljerind <abaljerinda@gmail.com>
2025-01-15 14:11:37 +0700

    Receive selectedFriend props
    Use selectedFriend props with it's property in h2, label and option tag

--- reactjs/split-bill/src/components/FormSplitBill.jsx
+++ reactjs/split-bill/src/components/FormSplitBill.jsx
@@ -1,7 +1,7 @@
-export default function FormSplitBill() {
+export default function FormSplitBill({ selectedFriend }) {
   return (
     <form action="" className="form-split-bill">
-      <h2>Patungan Bareng X</h2>
+      <h2>Patungan Bareng {selectedFriend.name}</h2>
 
       <label htmlFor="">💵Total Tagihan</label>
       <input type="text" />
@@ -9,13 +9,13 @@ export default function FormSplitBill() {
       <label htmlFor="">🙋‍♂️Tagihan Kamu</label>
       <input type="text" />
 
-      <label htmlFor="">🙎Tagihan X</label>
+      <label htmlFor="">🙎Tagihan {selectedFriend.name}</label>
       <input type="text" disabled />
 
       <label htmlFor="">🤑Dibayarin sama</label>
       <select name="" id="">
         <option value="user">Kamu</option>
-        <option value="friend">X</option>
+        <option value="friend">{selectedFriend.name}</option>
       </select>
 
       <button className="button">Tambah</button>
