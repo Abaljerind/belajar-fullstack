c79b24bfc5e5f4d01d084b629e8330b136a0f4f2
Abaljerind <abaljerinda@gmail.com>
2025-01-16 21:51:40 +0700

    Import PropTypes from prop-types package
    Create PropTypes rule for StarRating props & Add default value for size

--- reactjs/popmovie/src/StarRating.jsx
+++ reactjs/popmovie/src/StarRating.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -42,7 +43,13 @@ function Star({ onRate, fullRate, onHoverIn, onHoverOut, color, size }) {
   );
 }
 
-export default function StarRating({ max = 5, color, size }) {
+StarRating.propTypes = {
+  max: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+};
+
+export default function StarRating({ max = 5, color, size = 24 }) {
   const [rating, setRating] = useState(0);
   const [temporaryRating, setTemporaryRating] = useState(0);
 
