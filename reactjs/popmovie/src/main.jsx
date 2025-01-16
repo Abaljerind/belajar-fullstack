c9f6662234b141e4e40e3ee0482068e45023c558
Abaljerind <abaljerinda@gmail.com>
2025-01-16 21:52:55 +0700

    Comment App component and replace it with StarRating
    Send different value to StarRating via props

--- reactjs/popmovie/src/main.jsx
+++ reactjs/popmovie/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import StarRating from "./StarRating.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <App /> */}
+    <StarRating color="#fcc419" size={32} />
+    <StarRating max={10} color="teal" size={56} />
+    <StarRating max={7} color={"skyblue"} size={40} />
+  </StrictMode>
+);
