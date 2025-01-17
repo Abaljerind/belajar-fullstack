4ed6ec63c2a852431682fdac93ab5a1c18457676
Abaljerind <abaljerinda@gmail.com>
2025-01-17 21:08:22 +0700

    Comment StarRating instances and uncomment App instance

--- reactjs/popmovie/src/main.jsx
+++ reactjs/popmovie/src/main.jsx
@@ -6,9 +6,9 @@ import StarRating from "./StarRating.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
-    <StarRating color="#fcc419" size={32} />
+    <App />
+    {/* <StarRating color="#fcc419" size={32} />
     <StarRating max={10} color="teal" size={56} />
-    <StarRating max={7} color={"skyblue"} size={40} />
+    <StarRating max={7} color={"skyblue"} size={40} /> */}
   </StrictMode>
 );
